aaca39f2832e47380de2f864afae69234c8423ba
Kdddru <dhk9817@gmail.com>
2023-08-09 17:31:41 +0900

    0809

--- Data_Type/복사하기.js
+++ Data_Type/복사하기.js
@@ -1,6 +1,11 @@
 var user ={
     name : 'Dudu',
-    gender : 'male'
+    gender : 'male',
+    urls:{
+        portfolio : 'http://github.com/abc',
+        blog : 'http://blog.com',
+        facebook: 'http://facebook.com/abc'
+    }
 }
 
 //얕은 복사
@@ -12,12 +17,18 @@ var copyObject = function(target){
     return result;
 };
 
-
-console.log(copyObject(user));
-
 var user2 = copyObject(user);
-user2.name = 'Duru'
+
+//깊은 복사 ( 객체안의 객체 복사)
+//urls도 복사해야지 기존데이터 불변
+user2.urls = copyObject(user.urls);
 
 console.log(user2);
 
 
+user2.name = 'Duru';
+console.log(user.name === user2.name);
+
+//
+user.urls.portfolio ='http://portfolio.co.kr';
+console.log(user.urls.portfolio === user2.urls.portfolio) ;
